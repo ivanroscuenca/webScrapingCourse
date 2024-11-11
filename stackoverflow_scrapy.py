484f2371716f46bf813ca1162f31a770d793d326
ivan <ivanros@protonomail.com>
2024-11-11 14:59:24 +0100

    update last changes

--- stackoverflow_scrapy.py
+++ stackoverflow_scrapy.py
@@ -5,9 +5,12 @@ from scrapy.spiders import Spider
 from scrapy.selector import Selector
 from scrapy.loader import ItemLoader
 
+
 class Pregunta(Item):
-   pregunta = Field()
-   descripcion = Field()
+    pregunta = Field()
+    descripcion = Field()
+    id = Field()
+
 
 class stackoverflowSpider(Spider):
     name = 'miprimerSpider'
@@ -16,15 +19,27 @@ class stackoverflowSpider(Spider):
     }
     start_urls = ['https://stackoverflow.com/questions']
 
-    def parse(self,response):
+    # Funcion que se va a llamar cuando se haga el requerimiento a la URL semilla
+    def parse(self, response):
+        # Selectores: Clase de scrapy para extraer datos
         sel = Selector(response)
-        preguntas = sel.xpath('//div[@id="questions"]//div[@class="question-summary"]')
-
+        titulo_de_pagina = sel.xpath('//h1/text()').get()
+        print(titulo_de_pagina)
+        # Selector de varias preguntas
+        preguntas = sel.xpath(
+            '//div[@id="questions"]//div[contains(@class,"s-post-summary ")]')
+        i = 0
         for pregunta in preguntas:
-            item = ItemLoader(Pregunta(),pregunta)
-            item.add_xpath('pregunta','.//h3/a/text()')
-            item.add_xpath('descripcion', './/div/text()')
+            item = ItemLoader(Pregunta(),
+                              pregunta)  # Instancio mi ITEM con el selector en donde estan los datos para llenarlo
 
-            yield item.load_item()
+            # Lleno las propiedades de mi ITEM a traves de expresiones XPATH a buscar dentro del selector "pregunta"
+            item.add_xpath('pregunta', './/h3/a/text()')
+            item.add_xpath('descripcion',
+                           './/div[@class="s-post-summary--content-excerpt"]/text()')
+            item.add_value('id', i)
+            i += 1
+            yield item.load_item()  # Hago Yield de la informacion para que se escriban los datos en el archivo
 
-
+    # EJECUCION EN TERMINAL:
+    # scrapy runspider stackoverflow_scrapy.py -o resultados.csv
